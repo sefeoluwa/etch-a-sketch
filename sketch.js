82919a2a23dae498341c4904c07919ef381a5b7d
sefeoluwa <sefeoluwa01@gmail.com>
2023-02-06 19:07:48 +0100

    Program outline

--- sketch.js
+++ sketch.js
@@ -1,1 +1,14 @@
-const container = document.getElementById('container')
+//getting the main div where the square divs would be stored. 
+const container = document.getElementById('container');
+
+//function to create the divs
+
+//creating classlist for each div to be stlyed with css
+
+//make squares generate colors on hover
+
+//function to make a button generate random colors with cursor on hover
+
+//function to make button clear all highlights
+
+//function to make button erase specific squares on hover
