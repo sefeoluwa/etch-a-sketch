d4f695523434cd44c817d6b4cc27c484800d37a4
sefeoluwa <sefeoluwa01@gmail.com>
2023-02-22 03:08:10 +0100

    declarations and prompt text

--- sketch.js
+++ sketch.js
@@ -1,20 +1,38 @@
-//getting the main div where the square divs would be stored. 
-let container = document.getElementById('#container');
+//Get and declare the necessary html elements to be manipulated
+let containerDiv = document.getElementById('container');
+let userValue = document.getElementById('user-number');
+let userSubmit = document.getElementById('user-submit');
+let copyInput = document.getElementById('copy-input');
+let promptText = document.getElementById('prompt');
+let clearButton = document.getElementById('clear-all')
 
-//create a singular div
-let div = document.createElement('div');
-div.setAttribute('class', 'divs')
-
-//create the divs
-function createDiv(){
-    
-    
-    //loop the creation of the squares
- for (let i=0; i<256; i++){
-    
+//Give the user hint on entering grid size in the text box
+function entryHint(){
+    promptText.textContent = "Enter a number between 2 and 99.";
 }
 
+
+//create a function that makes the grid per user input or on page load
+function createDiv() {
+    let number = userValue.value;
+    if (number < 2 || number > 99 || isNaN(number)){
+        promptText.textContent = "Make sure the number entered is between 2 and 99!";
+    } else {
+        promptText.textContent = "";
+        userValue.textContent = ""
+        copyInput.textContent = "";
+        containerDiv.innerHTML = ""
+        if(number < 2 || number > 99 || number == ""){
+            for(let i = 0; i < 16; i++){
+                
+            }
+        }
+    }
+
+
 }
+    
+   
 
 
 
